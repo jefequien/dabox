af9301b617197f9d628f99c4016d5c64b89fd18a
Jeffrey Hu <hujh14@gmail.com>
2024-03-13 17:50:23 -0700

    cleanup

--- dabox/util/subprocess.py
+++ dabox/util/subprocess.py
@@ -5,41 +5,21 @@ import subprocess
 import sys
 from typing import Any, Sequence
 
-from rich import console
-
-CONSOLE = console.Console()
+from .logging import logger
 
 
-def run_command(command: str, background: bool = False) -> str:
+def run_command(command: str) -> str:
     """Run a command kill actions if it fails
 
     Args:
         command: Command to run.
-        background: Whether to background the command.
     """
-    if background:
-        subprocess.Popen(command, shell=True)
-        return ""
-
     res = subprocess.run(
         command, shell=True, capture_output=True, text=True, check=False
     )
     ret_code = res.returncode
     if ret_code != 0:
-        CONSOLE.print(f"[bold red]Error: `{command}` failed.")
-        sys.exit(1)
-    return res.stdout
-
-
-def run_command_and_capture_output(
-    command: str,
-) -> str:
-    res = subprocess.run(
-        command, shell=True, capture_output=True, text=True, check=False
-    )
-    ret_code = res.returncode
-    if ret_code != 0:
-        CONSOLE.print(f"[bold red]Error: `{command}` failed.")
+        logger.error(f"[bold red]Error: `{command}` failed.")
         sys.exit(1)
     return res.stdout
 
