65ac7f21e890b2c5bc07c67b3de01dad100ca510
Jeffrey Hu <hujh14@gmail.com>
2024-03-14 10:10:00 -0700

    minor

--- dabox/streams/mediamtx.py
+++ dabox/streams/mediamtx.py
@@ -46,7 +46,7 @@ def get_ffmpeg_commands() -> dict[str, str]:
     for stream_name, device_name in stream_mapping.items():
         ffmpeg_cmd = (
             f"ffmpeg -f {FFMPEG_INPUT_FORMAT} -loglevel error -framerate {frame_rate} -video_size {video_size} -pix_fmt {pixel_format} -i /dev/video0"
-            + " -preset ultrafast -tune zerolatency  -pix_fmt rgb24 -pkt_size 921600 -f rawvideo zmq:tcp://127.0.0.1:5556"
+            + " -preset ultrafast -tune zerolatency -pix_fmt rgb24 -pkt_size 921600 -f rawvideo zmq:tcp://127.0.0.1:5556"
             + " -preset ultrafast -tune zerolatency -b:v 1M -vcodec libx264 -bf 0 -f rtsp rtsp://localhost:8554/camera0"
         )
         ffmpeg_commands[stream_name] = ffmpeg_cmd
