743ce385e85b3fc764f0b0cda3fffa4c4ac05c35
Jeffrey Hu <hujh14@gmail.com>
2024-03-14 12:38:57 -0700

    Change logo (#13)

--- dabox/util/cli_logo.py
+++ dabox/util/cli_logo.py
@@ -9,13 +9,6 @@ _|_|_|    _|    _|  _|_|_|      _|_|    _|      _|
 """
 
 
-def _nice_print(msg, last=False):
-    print()
-    print("\033[0;31m" + msg + "\033[0m")
-    if last:
-        print()
-
-
 def cli_logo():
     """Print logo nicely."""
-    _nice_print(DABOX_LOGO, last=True)
+    print("\033[0;92m" + DABOX_LOGO + "\033[0m")
